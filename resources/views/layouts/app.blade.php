5b260fd99f6645ebed81ed5ce70fa6bc7b123ec3
sharbel93 <sharbel.chris@gmail.com>
2018-05-16 11:04:37 +0300

    add footer ui options

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     {{-- awesome hii tempalte ni wazuri sana, ata wametupa common tag place main staff zime anza --}}
 
     <!--Main container start -->
-    <main class="main-container">
+    <main class="main-container" style="margin-bottom: 30px;">
             
             @yield('content')
 
@@ -42,43 +42,11 @@
 
     </main>
 
-<<<<<<< HEAD
+
 {{-- Footer html source start--}}
     @include('partials.layouts._footer')
     {{-- Footer html source end--}}
-=======
-    <footer class="footer" >
-        <div class="main-content">
-            <div class="container">
-            <div class="row">
-                <div class="col-sm-6 text-center-mobile">
-                    <h3 class="white">{{env('APP_NAME')}}</h3>
-                    <h5 class="light regular light-white">Share code.</h5>
-                </div>
-                <div class="col-sm-6 text-center-mobile">
-                    <div class="row opening-hours" style="padding: 10px;">
-                        <h3>About {{env('APP_NAME')}}</h3>
-                        <p>our about here</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row bottom-footer text-center-mobile">
-                <div class="col-sm-8">
-                    <p>&copy; {{date('Y')." ".env('APP_NAME')}} | Powered By <a style="color:cyan; text-decoration: none;" href="http://www.appslab.co.ke" target="_blank">App:Lab KE</a></p>
-                </div>
-                <div class="col-sm-4 text-right text-center-mobile">
-                    <ul class="social-footer">
-                        <li><a target="_blank" href="http://www.facebook.com/pages/Codrops/159107397912"><i class="fa fa-bitbucket"></i></a></li>
-                        <li><a target="_blank" href="http://www.twitter.com/appslab"><i class="fa fa-twitter"></i></a></li>
-                        <li><a target="_blank" href="https://github.com/GeeksTalk/gtt_web"><i class="fa fa-github"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        </div>
 
-    </footer>
->>>>>>> da41c7c2ba43216d56b7c0bb46b86b354a05221f
 
     <!-- Register popup html source start -->
     @include('partials.layouts._reg')
--- resources/views/partials/layouts/_footer.blade.php
+++ resources/views/partials/layouts/_footer.blade.php
@@ -1,33 +1,67 @@
-<footer class="footer">
+{{--<footer class="footer" >--}}
+    {{--<div class="main-content">--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-sm-6 text-center-mobile">--}}
+                    {{--<h3 class="white">{{env('APP_NAME')}}</h3>--}}
+                    {{--<h5 class="light regular light-white">Share code.</h5>--}}
+                {{--</div>--}}
+                {{--<div class="col-sm-6 text-center-mobile">--}}
+                    {{--<div class="row opening-hours" style="padding: 10px;">--}}
+                        {{--<h3>About {{env('APP_NAME')}}</h3>--}}
+                        {{--<p>our about here</p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="row bottom-footer text-center-mobile">--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<p>&copy; {{date('Y')." ".env('APP_NAME')}} | Powered By <a style="color:cyan; text-decoration: none;" href="http://www.appslab.co.ke" target="_blank">App:Lab KE</a></p>--}}
+                {{--</div>--}}
+                {{--<div class="col-sm-4 text-right text-center-mobile">--}}
+                    {{--<ul class="social-footer">--}}
+                        {{--<li><a target="_blank" href="http://www.facebook.com/pages/Codrops/159107397912"><i class="fa fa-bitbucket"></i></a></li>--}}
+                        {{--<li><a target="_blank" href="http://www.twitter.com/appslab"><i class="fa fa-twitter"></i></a></li>--}}
+                        {{--<li><a target="_blank" href="https://github.com/GeeksTalk/gtt_web"><i class="fa fa-github"></i></a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+{{--</footer>--}}
+
+<footer class="footer" >
     <div class="main-content">
         <div class="main-content-wrapper">
             <div class="columns column-2">
-               <ul>
-                   <li>a</li>
-                   <li>a</li>
-                   <li>a</li>
-               </ul>
+                <p><h1>Logo</h1></p>
+              <p>Geek Talk Thursday</p>
             </div>
             <div class="columns column-2">
                 <ul>
-                    <li>a</li>
-                    <li>a</li>
-                    <li>a</li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Join</a></li>
+                    <li><a href="#">Authors</a></li>
+                    <li><a href="#">Posts</a></li>
+
                 </ul>
             </div>
             <div class="columns column-2">
-                <ul>
-                    <li>a</li>
-                    <li>a</li>
-                    <li>a</li>
+                <p>Contact Us</p>
+                <ul class="social-footer">
+                <li><a target="_blank" href="http://www.facebook.com/pages/Codrops/159107397912"><i class="fa fa-bitbucket"></i></a></li>
+                <li><a target="_blank" href="http://www.twitter.com/appslab"><i class="fa fa-twitter"></i></a></li>
+                <li><a target="_blank" href="https://github.com/GeeksTalk/gtt_web"><i class="fa fa-github"></i></a></li>
                 </ul>
             </div>
-            <div class=" column-6">
-                <div class="content-b">
-                    <p style="text-align: center; padding: 10px;" >{{date('Y')." ".env('APP_NAME')}} | Powered By <a style="color:cyan; text-decoration: none;" href="http://www.appslab.co.ke" target="_blank">App:Lab KE</a> </p>
+            <div class="row">
+                <div class="col-12 col-md-12">
+                    <div class="content-b">
+                        <p style="text-align: center; padding: 10px;" >{{date('Y')." ".env('APP_NAME')}} | Powered By <a style="color:cyan; text-decoration: none;" href="http://www.appslab.co.ke" target="_blank">App:Lab KE</a> </p>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 </footer>
