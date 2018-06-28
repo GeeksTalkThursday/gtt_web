bb48ec03c50b640dca59189948870549c52c2a61
appslab_moi <marvincollins14@gmail.com>
2018-06-28 11:12:29 +0300

    ...

--- resources/views/partials/layouts/_top_nav.blade.php
+++ resources/views/partials/layouts/_top_nav.blade.php
@@ -70,7 +70,7 @@
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
+                                            Logout
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
